3e1f2e402dfacd727c1563c9a1e3682a391fa94e
susiexia <57109686+susiexia@users.noreply.github.com>
2020-01-31 23:34:42 -0800

    scrape featured image

--- Mission_to_Mars.py
+++ Mission_to_Mars.py
@@ -34,4 +34,30 @@ news_p
 # ## Web Scrape the Featured Images from NASA Mars spaceimages website
 
 # %%
+# use browser auto visit SpaceImage website
 image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(image_url)
+
+# directly use browser functions, not transfer to html now.
+
+# STEP 1: click by id = 'full_image' and assign to a variable
+full_image_elem = browser.find_by_id('full_image', wait_time=1)
+full_image_elem.click()
+
+# STEP 2: click by text 'more info' and assign to a variable
+browser.is_element_present_by_text('more info', wait_time=1) # make sure if text in page
+more_info_elem = browser.click_link_by_partial_text('more info')
+
+# STEP 3:use BeautifulSoup to parse 
+html = browser.html
+image_soup = BeautifulSoup(html, 'html.parser')
+# use CSS selector in BeautifulSoup to extract img_url
+image_relative_url = image_soup.select_one('figure.lede a img').get('src')
+
+#combine with base URL to create an absolute img URL
+img_url = f'https://www.jpl.nasa.gov{image_relative_url}'
+img_url
+
+# check if a valid img_url
+#browser.visit(img_url)
+# %%
