9ff6869b4c2df26d21b26c6e1bd7c635641126c1
susiexia <57109686+susiexia@users.noreply.github.com>
2020-02-01 20:04:14 -0800

    create flask app connecting with MongoDB

---
+++ apps/app.py
@@ -0,0 +1,35 @@
+
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+import scraping  # self created python file
+
+# set up Flask 
+app = Flask(__name__)
+
+# config MongoDB URI with flask app
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app' #mars_app is DataBase name (=db)
+# set up mongodb connection
+mongo = PyMongo(app)
+
+# --------------------------------------------------------
+# set up flask app route of homepage(root)
+@app.route('/')
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars = mars)
+
+# ---------------------------------------------------------
+# set up scraping route
+# step_1: access the database, 
+# step_2: scrape newly scraped data using  'scrape_mars.py' script, 
+# step_3: update the database, and return a message when successful
+@app.route('/scrape')
+def scrape():
+    mars = mongo.db.mars 
+    mars_data = scrape_mars.scrape_all()
+    mars.update({}, mars_data, upsert = True)
+    return "Scraping Successful!"
+
+# run flask app using terminal
+if __name__ == '__main__':
+    app.run()   # run on local development server
